7be23a4dd98f6aed75badbe8d6206416d70b0326
Anyang Ateny <anyangateny@outlook.com>
2026-09-17 22:08:36 +0930

    feat: add email support to API

--- internal/awsservice/lambda_handler.go
+++ internal/awsservice/lambda_handler.go
@@ -7,27 +7,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 )
-
-// type config struct {
-// 	region      string
-// 	bucket_name string
-// }
-
-// type AwsGateway struct {
-// 	cfg config
-// 	s3  s3.Client
-// }
-
-type RequestBody struct {
-	Name string `json:"name"`
-}
-
-type ResponseBody struct {
-	Message string `json:"message"`
-}
 
 func HandleRequest(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	s3Client, err := newS3Client(
@@ -35,21 +19,15 @@ func HandleRequest(ctx context.Context, req events.APIGatewayV2HTTPRequest) (eve
 		"anyang-personal-website",
 	)
 	if err != nil {
-		log.Fatalf("Could not start S3 client, %w", err)
+		log.Fatal("Could not start S3 client, %w", err)
 	}
 
 	switch {
 	case req.RequestContext.HTTP.Method == "GET" && req.RawPath == "/projects":
 		return getProjects(ctx, req, s3Client)
 
-	// case req.RequestContext.HTTP.Method == "GET" && req.RawPath == "/image":
-	// 	return getImage(ctx, req, s3Client)
-	//
-	// case req.RequestContext.HTTP.Method == "GET" && req.RawPath == "/resume":
-	// 	return getResume(ctx, req)
-	//
-	// case req.RequestContext.HTTP.Method == "POST" && req.RawPath == "/contact":
-	// 	return postContact(ctx, req)
+	case req.RequestContext.HTTP.Method == "POST" && req.RawPath == "/contact":
+		return postContact(ctx, req)
 
 	default:
 		return events.APIGatewayV2HTTPResponse{
@@ -125,10 +103,78 @@ func getProjects(
 	}, nil
 }
 
-// func getImage(
-// 	ctx context.Context,
-// 	req events.APIGatewayV2HTTPRequest,
-// 	c *S3Client,
-// ) (events.APIGatewayV2HTTPResponse, error) {
-// 	signedImageUrl := req.RawPath
-// }
+type emailMessage struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"subject"`
+}
+
+func postContact(
+	ctx context.Context,
+	req events.APIGatewayV2HTTPRequest,
+) (events.APIGatewayV2HTTPResponse, error) {
+	// TODO: Split this into a SMTP config
+
+	host, ok := os.LookupEnv("SMTP_HOST")
+	if !ok {
+		log.Fatal("SMTP_HOST is not set")
+	}
+
+	username, ok := os.LookupEnv("SMTP_USER")
+	if !ok {
+		log.Fatal("SMTP_USER is not set")
+	}
+
+	password, ok := os.LookupEnv("SMTP_PASSWORD")
+	if !ok {
+		log.Fatal("SMTP_PASSWORD is not set")
+	}
+	auth := smtp.PlainAuth("", username, password, host)
+
+	var emailMessage emailMessage
+	if err := json.Unmarshal([]byte(req.Body), &emailMessage); err != nil {
+		return events.APIGatewayV2HTTPResponse{}, fmt.Errorf(
+			"failed to unmarshal email: %w",
+			err,
+		)
+	}
+
+	// TODO: Add more INFO, DEBUG logging
+	en := emailMessage.Name
+	ee := emailMessage.Email
+	em := emailMessage.Message
+
+	msgStr := fmt.Sprintf(
+		"Subject: atenyanyang.com\r\n"+
+			"\r\n"+
+			"New email from atenyanyang.com\r\n"+
+			"Name: %s\r\n"+
+			"Email: %s\r\n"+
+			"Message: %s",
+		en, ee, em,
+	)
+
+	msg := []byte(msgStr)
+
+	port, ok := os.LookupEnv("SMTP_PORT")
+	if !ok {
+		log.Fatal("SMTP_PORT is not set")
+	}
+
+	to := []string{os.Getenv("EMAIL_TO")}
+
+	from, ok := os.LookupEnv("EMAIL_FROM")
+	if !ok {
+		log.Fatal("EMAIL_FROM is not set")
+	}
+
+	addr := host + ":" + port
+	err := smtp.SendMail(addr, auth, from, to, msg)
+	if err != nil {
+		log.Fatal("Send mail failed, %w", err)
+	}
+
+	return events.APIGatewayV2HTTPResponse{
+		StatusCode: http.StatusOK,
+	}, nil
+}
